1739c066e7295c4e4812c76d5915ce67e6f511b8
felipe1297 <48300568+felipe1297@users.noreply.github.com>
2021-09-02 23:17:18 -0500

    Update GraphQLConfig.java

--- src/main/java/com/viwit/viwitinterface/infraestructure/driven_adapter/graphql/GraphQLConfig.java
+++ src/main/java/com/viwit/viwitinterface/infraestructure/driven_adapter/graphql/GraphQLConfig.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Builder(toBuilder = true)
 public class GraphQLConfig {
     //INVERSE_PROXY
-    public final String URL_CONNECTION = "https://54.237.94.101:8000/graphql";
+    public final String URL_CONNECTION = "http://54.237.94.101:8000/graphql";
 
 //     API_GATEWAY
 //     public final String URL_CONNECTION = "http://54.174.205.83:3000/graphql";
